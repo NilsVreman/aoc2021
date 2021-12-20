ef1ddb7355a5ea2862ab321da8fe3bd2a12cd6d9
NilsVreman <nils.vreman@gmail.com>
2021-12-20 16:09:57 +0100

    Updated shitty files

--- day01/target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-/local/home/nilsv/PhD/projects/advent-of-code-2021/nils/day01/target/debug/day01: /local/home/nilsv/PhD/projects/advent-of-code-2021/nils/day01/src/main.rs
--- day01/target/debug/deps/day01-fdb1117bfe369d7f.d
+++
@@ -1,5 +0,0 @@
-/local/home/nilsv/PhD/projects/advent-of-code-2021/nils/day01/target/debug/deps/day01-fdb1117bfe369d7f: src/main.rs
-
-/local/home/nilsv/PhD/projects/advent-of-code-2021/nils/day01/target/debug/deps/day01-fdb1117bfe369d7f.d: src/main.rs
-
-src/main.rs:
